ade201fe2fcdb20660d58ae22f07e37215b4ed6f
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 10:38:05 -0700

    varas done

--- Module1/LA1-1/vars.cpp
+++ Module1/LA1-1/vars.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main(){
+cout << "Data Types   Bytes" 
+     << "\n----------  ------"
+     << "\nint              "<<sizeof(int)
+     << "\nchar             "<<sizeof(char)
+     << "\nlong             "<<sizeof(long)
+     << "\nbool             "<<sizeof(bool)
+     << endl;
+
     return 0;
 }
