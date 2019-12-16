a86c6965cd7ac4e2c2efaa28d291c961244f217c
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 11:27:48 -0700

    evaluated bmi

--- Module1/LA1-1/bmi.cpp
+++ Module1/LA1-1/bmi.cpp
@@ -10,6 +10,8 @@ using namespace std;
 const float kMetersToInch = 39.37;
 const float kKiloToLbs = 2.204;
 const int kBMIImperial = 703;
+const float lowBmi = 18.5;
+const float hiBmi = 24.9;
 
 int main(){
     float hight = 0;    // In meters
@@ -20,12 +22,27 @@ int main(){
     cin >> hight;
     cout << "what is your weight? (in Kg) ";
     cin >> weight;
+    
     bmi =  weight / (hight * hight);
     cout << "Your BMI is: " << bmi << ".  You need to get off your ass." << endl;
-    weight *= kMetersToInch;
-    hight *= kKiloToLbs; 
+    
+    hight *= kMetersToInch;
+    weight *= kKiloToLbs; 
     bmi = (weight * kBMIImperial)/(hight*hight);
     cout <<"In (imperal) your BMI is: " << bmi << ".  It's a lieeee!!!!" << endl;
     
+    if(bmi >= lowBmi && bmi < hiBmi) {
+        cout << "Your Bmi is good and between " << lowBmi << " and " << hiBmi << "." << endl;
+    } else {
+        if (bmi < lowBmi){
+            cout << "Your bmi is bellow " << lowBmi << ".  You should eat more candy!" << endl;
+        }
+        else {
+            cout << "Your bmi is above " << hiBmi << ".  You should stop eating candy!" << endl;
+        }
+    }
+
+    cout << "Thanks for using BMI" << endl;
+
     return 0;
 }
