750aa17b4523a1d0dc351a26152b42beb1ac6919
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 09:52:55 -0700

    stuff for fun

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main() {
+    unsigned int age;
+    // comits are junk don't use tham.
     cout << "Hello all" << endl;
     cout << "Becase I can" << " and I want too" << endl;
+
+    cout << "Age? " ;
+    cin >> age;
+    if (age > 20) {
+        cout << "your age is: " << age << ". You ARE VERY OLD!!!" << endl;
+    }
+    else
+    {
+        cout << "your age is: " << age << ". You ARE VERY Young!!!" << endl;
+    }
+        
+    
     return 0;
+    // All hail clean code!!
  }
