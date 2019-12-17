d3822c00c1a344e80a27be4847bb1f515fc03f0a
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-17 10:50:38 -0700

    more stuff

--- Module1/LA1-3/main.cpp
+++ Module1/LA1-3/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "util.h"
+
+using namespace std;
 
 // Main Function
 int main()
 {
+  int x = 6, y = 5;
+  cout << "the sum of " << x << " and " << y << " is: " << sum(x,y) << "and x = " << x << " y = " << y << endl;
   return 0;
 }
---
+++ Module1/LA1-3/util.h
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2019
+** hafb-cpp-fundamentals
+** File description:
+** util
+*/
+
+#pragma once
+
+int sum(int x, int y);
